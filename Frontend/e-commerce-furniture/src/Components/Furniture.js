e42fd32c28d7e773dfec746760e399683a1f7d14
muhammad suhail <suhailpk539@gmail.com>
2024-05-29 19:13:33 +0530

    .

--- Frontend/e-commerce-furniture/src/Components/Furniture.js
+++ Frontend/e-commerce-furniture/src/Components/Furniture.js
@@ -23,6 +23,7 @@ const Furniture = () => {
        useEffect(() => {
        fetchProducts()
     }, [])
+    //   console.log("product c:",product.category);
     const filteredProducts = product.filter(product => product.category === type.type)
     // console.log("FP",filteredProducts);
   return (
