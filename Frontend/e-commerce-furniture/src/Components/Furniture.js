5d652619a472f9b86f1bdda8d17a75882b38ca50
muhammad suhail <suhailpk539@gmail.com>
2024-05-29 19:32:25 +0530

    .

--- Frontend/e-commerce-furniture/src/Components/Furniture.js
+++ Frontend/e-commerce-furniture/src/Components/Furniture.js
@@ -19,8 +19,8 @@ const Furniture = () => {
         const fetchProducts = async () => {
             try {
                 const response = await axios.get("https://mern-stack-build-mart-nqt8.vercel.app/api/users/products");
-                setProduct(response.data.data);
-                console.log("Response: ", response);
+                setProduct(response)
+                // console.log("Response: ", response);
             } catch (error) {
                 console.error("Error fetching products: ", error);
                 setProduct([]); // Ensure product is an array even if there's an error
