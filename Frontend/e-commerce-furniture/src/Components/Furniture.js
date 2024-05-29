1355581e7c84c883a2f08dd7a7ae3c3ce8df0f43
muhammad suhail <suhailpk539@gmail.com>
2024-05-29 17:03:21 +0530

    .

--- Frontend/e-commerce-furniture/src/Components/Furniture.js
+++ Frontend/e-commerce-furniture/src/Components/Furniture.js
@@ -18,7 +18,7 @@ const Furniture = () => {
                 const response = await axios.get("https://mern-stack-build-mart-nqt8.vercel.app/api/users/products", userConfig).then((res)=>{
                   console.log(res);
                 })
-                setProduct(response.data);
+                // setProduct(response.data);
                 console.log(response.data);
             } catch (error) {
                 console.error("Error fetching products:", error);
